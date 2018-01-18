a17c23359fd30977328d687b5eaa4db958b5b651
Onur Can <olmaditekrar@gmail.com>
2018-01-18 03:25:35 +0300

    Finished insertion and querying with MongoDB using Bottle

--- restful.py
+++ restful.py
@@ -1,29 +1,72 @@
 from bottle import get,run,post,request,delete
+from pymongo import MongoClient
+import pprint
+import json
+def get_db(db_name):
+    client = MongoClient('localhost:27017')
+    db = client[db_name]
+    return db
 
-peopleNames = [{'name':'OnurCan' , 'surname' : 'Yucedag','age':'20'},
-           {'name': 'Ali', 'surname': 'Avci','age':'21'},
-           {'name': 'Sevgi', 'surname': 'Cetin','age':'21'}]
+db = get_db('examples')
+projection = {'_id': 0, 'name': 1,'surname':1,'age':1}
 
-@get(' /people')
+def findMongo(db,query=None):
+    # For local use
+    results = db.bottleExample.find(query,projection)
+    dict = []
+    for record in results:
+        dict.append(record)
+    return (dict)
+
+@get('/people/')
 def getAll():
-    return {'people' : peopleNames}
+    return {'people' : findMongo(db)}
+
+
+
+
 
 @get('/people/<name>')
 def getOne(name):
-    the_name = [person for person in peopleNames if person['name'] == name]
-    return {'person' : the_name[0]}
+    the_name = findMongo(db,{'name': name })
+    return {'person' : the_name}
+
+
+
 
 @post('/people/')
 def addOne():
     new_person = {'name' : request.json.get('name'),
                   'surname' : request.json.get('surname'),
                   'age' : request.json.get('age')}
-    peopleNames.append(new_person)
-    return {'people' : peopleNames}
+    if insertMongo(db,new_person) :
+        return {'people' : findMongo(db)}
+    else:
+        return {'ERROR!':'There is already same record!'}
+
+def insertMongo(db,data):
+
+    if findMongo(db,data) != None:
+        print('There is already same record.')
+        return False
+    else:
+        db.bottleExample.insert(data)
+        print(data)
+        return True
+
+
+
+
 @delete('/people/<name>')
 def deleteOne(name):
-    deleted_person = [person for person in peopleNames if person['name'] == name] #Searching for name.
-    peopleNames.remove(deleted_person[0])
-    return {'people' : peopleNames}
+    deleted_person = findMongo(db,{'name':name}) #Searching for name.
+    deleteMongo(data=deleted_person)
+    return {'people' : findMongo(db,None)}
+def deleteMongo(data):
+    db.bottleExample.remove(data)
+
+
+
 
 run(reloader=True, debug= True)
+
